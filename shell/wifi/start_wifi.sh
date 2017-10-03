d213a6ac48f30a409ba093a41462b871968509b7
decren <xiaoershiwo@126.com>
2017-10-03 10:41:11 +0800

    Update start_wifi.sh

--- shell/wifi/start_wifi.sh
+++ shell/wifi/start_wifi.sh
@@ -1,3 +1,3 @@
 ip link set wlp2s0 up
-wpa_supplicant -B -i wlp2s0 -c<(wpa_passphrase "annie" "15019229827")
+wpa_supplicant -B -i wlp2s0 -c<(wpa_passphrase "wifi_name" "wifi_password")
 dhclient wlp2s0
