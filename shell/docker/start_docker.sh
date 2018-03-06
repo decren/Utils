2619f16b264874f4d1f5dc4c8f183159b1f94d20
decren <595472968@qq.com>
2018-03-06 21:52:47 +0800

    port mapping

--- shell/docker/start_docker.sh
+++ shell/docker/start_docker.sh
@@ -3,4 +3,4 @@ CMD="/bin/bash";
 if [ $# -ge 2 ];then
 	CMD=$2;
 fi
-docker run -itd --privileged=true -v /home/docker_share:/docker_share $image_name $CMD
+docker run -itd --privileged=true -p58080:8080 -v/home/docker_share:/docker_share $image_name $CMD
