f3f322b4c877896d84c5d621c23ad18135c1a003
decren <595472968@qq.com>
2018-08-27 17:53:04 +0800

    add help

--- shell/docker/start_docker.sh
+++ shell/docker/start_docker.sh
@@ -1,6 +1,14 @@
-image_name=$1
-CMD="/bin/bash";
-if [ $# -ge 2 ];then
-	CMD=$2;
+function help()
+{
+	echo "please input image_name container_name"
+}
+
+if [ $# -lt 2 ];then
+	help
+	exit -1;
 fi
-docker run -itd --privileged=true -p58080:8080 -v/home/docker_share:/docker_share $image_name $CMD
+
+image_name=$1
+container_name=$2
+CMD="/bin/bash";
+docker run --name ${container_name} -itd --privileged=true -p58080:8080 -v/home/docker_share:/docker_share $image_name $CMD
